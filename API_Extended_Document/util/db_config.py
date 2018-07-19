dcd0215072f45cc253d4d3304d8402d4e3f26d4b
laurenttainturier <laurent.tainturier.pro@gmail.com>
2018-07-19 10:25:41 +0200

    Database password from environment variable

--- API_Extended_Document/util/db_config.py
+++ API_Extended_Document/util/db_config.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # coding: utf8
 
+import os
 import yaml
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -13,11 +14,18 @@ def get_db_info(config_file_name):
     try:
         with open(config_file_name, 'r') as file:
             config = yaml.load(file)
-
+        
+        # if the environment variable "EXTENDED_DOC_PASSWORD" exists
+        if os.environ.get("EXTENDED_DOC_PASSWORD"):
+            password = os.environ["EXTENDED_DOC_PASSWORD"]
+        # otherwise we use the default password from 'config.yml'
+        else:
+            password = config["password"]
+        
         db_info = \
             config['ordbms'] + "://" + \
             config["user"] + ":" + \
-            config["password"] + "@" + \
+            password + "@" + \
             config["host"] + ":" + \
             str(config["port"]) + "/" + \
             config["dbname"]
